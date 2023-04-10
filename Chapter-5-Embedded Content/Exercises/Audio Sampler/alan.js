54e5d61569711904f5c74d1a433aed308d797b5a
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2023-04-11 01:03:41 +0400

    chapter 5
    I have completed chapter 5's bonus exercise

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/alan.js
@@ -0,0 +1,61 @@
+var audio = new Audio();
+audio.src = "Audio/ah-ha.mp3";
+
+var audio2 = new Audio();
+audio2.src = "Audio/back-of-the-net.mp3";
+
+var audio3 = new Audio();
+audio3.src = "Audio/bangoutoforder.mp3";
+
+var audio4 = new Audio();
+audio4.src = "Audio/dan.mp3";
+
+var audio5 = new Audio();
+audio5.src = "Audio/emailoftheevening.mp3";
+
+var audio6 = new Audio();
+audio6.src = "Audio/hellopartridge.mp3";
+
+var audio7 = new Audio();
+audio7.src = "Audio/iateascotchegg.mp3";
+
+var audio8 = new Audio();
+audio8.src = "Audio/imconfused.mp3";
+
+var audio9 = new Audio();
+audio9.src = "Audio/yikes.mp3";
+
+var audio10 = new Audio();
+audio10.src = "Audio2/a-man-has-fallen-into-the-river.mp3";
+
+var audio11 = new Audio();
+audio11.src = "Audio2/Auuuuugh.mp3";
+
+var audio12 = new Audio();
+audio12.src = "Audio2/BABAHAFT.mp3";
+
+var audio13 = new Audio();
+audio13.src = "Audio2/fool.mp3";
+
+var audio14 = new Audio();
+audio14.src = "Audio2/1000yearsofpain.mp3";
+
+var audio15 = new Audio();
+audio15.src = "Audio2/Neither.mp3";
+
+var audio16 = new Audio();
+audio16.src = "Audio2/wow.mp3";
+
+var audio17 = new Audio();
+audio17.src = "Audio2/Yeet.mp3";
+
+var audio18 = new Audio();
+audio18.src = "Audio2/Howabsurd.mp3";
+
+let btn = document.getElementById("speechboxbutton");
+let spc = document.getElementById("texttospeech");
+
+btn.addEventListener("click", () => {
+  const utterThis = new SpeechSynthesisUtterance(spc.value);
+  window.speechSynthesis.speak(utterThis);
+});
