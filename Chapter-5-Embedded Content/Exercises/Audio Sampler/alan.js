340330c579fc87bffe1a076e9ebdd2a9d1ae9d39
Zoraiz Mudassar <114096672+Zeko54@users.noreply.github.com>
2023-04-27 11:57:24 +0400

    chapter 5
    i have completed all the exercises in chapter 5 and have also commented in them

--- Chapter-5-Embedded Content/Exercises/Audio Sampler/alan.js
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/alan.js
@@ -1,6 +1,8 @@
+// making an audio variable and creating a new audio object. setting the audio's soure path for the audio file.
 var audio = new Audio();
 audio.src = "Audio/ah-ha.mp3";
 
+//repeating the same process and giving new variable for new audios
 var audio2 = new Audio();
 audio2.src = "Audio/back-of-the-net.mp3";
 
@@ -49,12 +51,17 @@ audio16.src = "Audio2/wow.mp3";
 var audio17 = new Audio();
 audio17.src = "Audio2/Yeet.mp3";
 
+//doing this for a total of 18 audios
 var audio18 = new Audio();
 audio18.src = "Audio2/Howabsurd.mp3";
 
+//making a new variable called 'btn' and 'spc'. referencing 'btn' to  button element with the id [speechboxbutton] and
+//referencing spc to textarea element with the id [texttospeech]
 let btn = document.getElementById("speechboxbutton");
 let spc = document.getElementById("texttospeech");
 
+//adding an event listener called click to 'btn', then creating a new SpeechSynthesisUtterance object using the value of the 'spc'
+//input element and assinging it to the 'utterThis' constant.lastly adding the speech api to speak the text represented by 'utterThis'.
 btn.addEventListener("click", () => {
   const utterThis = new SpeechSynthesisUtterance(spc.value);
   window.speechSynthesis.speak(utterThis);
